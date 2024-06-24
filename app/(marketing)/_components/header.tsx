7aea9cb5170a2bf2ddb3b046e1910e5dfb7c155a
prog-daiki <t.daiki0913@icloud.com>
2024-06-24 21:24:41 +0900

    ロゴにリンクを付与

--- app/(marketing)/_components/header.tsx
+++ app/(marketing)/_components/header.tsx
@@ -9,15 +9,18 @@ import {
 import { Loader2 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <>
       <header className="h-20 w-full border-b-2 border-slate-200">
         <div className="container flex h-full items-center justify-between">
-          <h1 className="text-2xl font-bold">
-            Code <span className="text-blue-600">.</span>
-          </h1>
+          <Link href="/">
+            <h1 className="text-2xl font-bold">
+              Code <span className="text-blue-600">.</span>
+            </h1>
+          </Link>
           <ClerkLoading>
             <Loader2 className="size-4 animate-spin text-muted-foreground" />
           </ClerkLoading>
