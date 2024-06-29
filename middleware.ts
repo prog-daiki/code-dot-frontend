fdfd59a54dbd4ae502e76670658f7a45d6a48492
prog-daiki <t.daiki0913@icloud.com>
2024-06-29 15:48:16 +0900

    ミドルウェアの修正

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher,
-} from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
