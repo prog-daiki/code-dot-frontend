94c8e68572b0b695b6c504e834b9863994fec266
prog-daiki <t.daiki0913@icloud.com>
2024-08-10 21:20:10 +0900

    検索ページの作成

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,11 +1,15 @@
 import { getCategories } from "@/data/category/get-categories";
 import { Categories } from "./_components/categories";
+import { SearchInput } from "./_components/search-input";
 
 export default async function DashboardPage() {
   const categories = await getCategories();
 
   return (
     <div>
+      <div className="mb-4">
+        <SearchInput />
+      </div>
       <Categories items={categories} />
     </div>
   );
