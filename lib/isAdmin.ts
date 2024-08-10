2e28ce5a3c30be5f62df73b86375e3fc501fcf1a
prog-daiki <t.daiki0913@icloud.com>
2024-08-10 20:26:54 +0900

    フォーマットの修正

--- lib/isAdmin.ts
+++ lib/isAdmin.ts
@@ -1,6 +1,4 @@
-export const isAdmin = async (
-  userId?: string | null,
-): Promise<boolean> => {
+export const isAdmin = async (userId?: string | null): Promise<boolean> => {
   const adminUserId = process.env.ADMIN_USER_ID;
   if (!adminUserId) {
     console.warn("管理者ユーザーIDが設定されていません");
