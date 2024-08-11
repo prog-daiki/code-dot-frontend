412c938f14dfbed6a480795eaf1bbbebc7dc210f
prog-daiki <t.daiki0913@icloud.com>
2024-08-11 14:09:35 +0900

    ログインしている場合ホームに飛ぶように修正

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,10 +1,15 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 type Props = {
   children: React.ReactNode;
 };
 
-export default function MarketingLayout({
-  children,
-}: Props) {
+export default function MarketingLayout({ children }: Props) {
+  const { userId } = auth();
+  if (userId) {
+    redirect("/home");
+  }
   return (
     <>
       <main>{children}</main>
