0fad1195cb0c2b3fe5fded4e9d0fbc0cdb5829a8
prog-daiki <t.daiki0913@icloud.com>
2024-08-11 11:58:41 +0900

    getCourseにコメントを付与

--- data/course/get-course.ts
+++ data/course/get-course.ts
@@ -4,17 +4,17 @@ import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export async function getCourse(
-  courseId: string,
-): Promise<Course> {
+/**
+ * 講座を取得する
+ * @param courseId 講座ID
+ * @returns 講座
+ */
+export async function getCourse(courseId: string): Promise<Course> {
   try {
     const token = await auth().getToken();
-    const response = await axios.get<Course>(
-      `${API_URL}/courses/${courseId}`,
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      },
-    );
+    const response = await axios.get<Course>(`${API_URL}/courses/${courseId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     return response.data;
   } catch (error) {
     console.error("講座の取得に失敗しました:", error);
