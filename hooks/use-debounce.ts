bc349e78d1ac7879332d971c9c461385e3f34baf
prog-daiki <t.daiki0913@icloud.com>
2024-08-11 12:06:25 +0900

    useDebounceにコメントを付与

--- hooks/use-debounce.ts
+++ hooks/use-debounce.ts
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
 
+/**
+ * デバウンスする
+ * @param value 値
+ * @param delay デバウンスするまでの時間
+ * @returns デバウンスされた値
+ */
 export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
