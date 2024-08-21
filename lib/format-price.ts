3f02cf4a9c1d0269b61460a64664fbb7b51b7e5a
prog-daiki <t.daiki0913@icloud.com>
2024-08-21 22:14:20 +0900

    format-priceを修正

--- lib/format-price.ts
+++ lib/format-price.ts
@@ -1,11 +1,13 @@
 /**
- * 金額をフォーマットする
- * @param price 金額
- * @returns フォーマットされた金額
+ * 金額を日本円形式でフォーマットする
+ * @param price フォーマットする金額（数値）
+ * @returns フォーマットされた金額（文字列）
  */
-export const formatPrice = (price: number) => {
-  return new Intl.NumberFormat("ja-JP", {
+export const formatPrice = (price: number): string => {
+  const formatter = new Intl.NumberFormat("ja-JP", {
     style: "currency",
     currency: "JPY",
-  }).format(price);
+  });
+
+  return formatter.format(price);
 };
