4c94ef169f33d9e24cd88263d6e7ee856fee82e5
prog-daiki <t.daiki0913@icloud.com>
2024-08-22 00:09:18 +0900

    get-coursesを修正

--- data/course/get-courses.ts
+++ data/course/get-courses.ts
@@ -1,22 +1,27 @@
 import { Course } from "@/types/course";
 import { auth } from "@clerk/nextjs/server";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 /**
  * 講座の一覧を取得する
  * @returns 講座の一覧
+ * @throws Error 講座の取得に失敗した場合
  */
 export async function getCourses(): Promise<Course[]> {
   try {
     const token = await auth().getToken();
-    const response = await axios.get<Course[]>(`${API_URL}/courses`, {
+    const { data } = await axios.get<Course[]>(`${API_URL}/courses`, {
       headers: { Authorization: `Bearer ${token}` },
     });
-    return response.data;
+    return data;
   } catch (error) {
-    console.error("講座の取得に失敗しました:", error);
-    throw error;
+    const errorMessage =
+      error instanceof AxiosError
+        ? `講座の一覧取得に失敗しました: ${error.message}`
+        : "講座の一覧取得中に予期せぬエラーが発生しました";
+    console.error(errorMessage);
+    throw new Error(errorMessage);
   }
 }
