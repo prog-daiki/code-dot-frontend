b589cf7c9986455ebc0ced250f00d83af1731f5e
prog-daiki <t.daiki0913@icloud.com>
2024-08-21 22:19:41 +0900

    use-debounceの修正

--- hooks/use-debounce.ts
+++ hooks/use-debounce.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "react";
 
 /**
- * デバウンスする
- * @param value 値
- * @param delay デバウンスするまでの時間
+ * 値をデバウンスするカスタムフック
+ * @param value デバウンスする値
+ * @param delay デバウンス時間（ミリ秒）
  * @returns デバウンスされた値
  */
-export function useDebounce<T>(value: T, delay: number): T {
+export function useDebounce<T>(value: T, delay = 500): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
     return () => clearTimeout(timer);
   }, [value, delay]);
 
