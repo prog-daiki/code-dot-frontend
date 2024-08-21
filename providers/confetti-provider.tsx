42b21ad4b9359a44fa19124f257e9a6e53b1779d
prog-daiki <t.daiki0913@icloud.com>
2024-08-21 22:08:57 +0900

    confetti-providerを修正

--- providers/confetti-provider.tsx
+++ providers/confetti-provider.tsx
@@ -4,9 +4,11 @@ import ReactConfetti from "react-confetti";
 import { useConfettiStore } from "@/hooks/use-confetti-store";
 
 export const ConfettiProvider = () => {
-  const confetti = useConfettiStore();
+  const { isOpen, onClose } = useConfettiStore();
 
-  if (!confetti.isOpen) return null;
+  if (!isOpen) {
+    return null;
+  }
 
   return (
     <>
@@ -14,7 +16,7 @@ export const ConfettiProvider = () => {
         className="pointer-events-none z-[100]"
         numberOfPieces={500}
         recycle={false}
-        onConfettiComplete={() => confetti.onClose()}
+        onConfettiComplete={onClose}
       />
     </>
   );
