658da9e38c702c1bc302eb5c4bb7c48b7c9d864a
prog-daiki <t.daiki0913@icloud.com>
2024-09-01 09:44:53 +0900

    マーケティングページのフォントサイズを修正

--- app/(marketing)/_components/call-action.tsx
+++ app/(marketing)/_components/call-action.tsx
@@ -6,11 +6,11 @@ import { ArrowRight } from "lucide-react";
 export const CallAction = () => {
   return (
     <div className="space-y-10">
-      <h2 className="text-md text-center text-black font-bold md:text-lg lg:text-2xl">
+      <h2 className="text-md text-center text-black font-bold text-lg lg:text-2xl">
         コードを書いて、人生を書き換える。
       </h2>
       <SignInButton forceRedirectUrl="/home" mode="modal">
-        <Button className="w-[300px] bg-sky-700 text-white hover:bg-sky-900 text-xl py-8 lg:text-2xl">
+        <Button className="w-[300px] bg-sky-700 text-white hover:bg-sky-900 text-lg py-8 lg:text-2xl">
           Code. で学ぶ
           <ArrowRight className="ml-2 w-6 h-6" />
         </Button>
