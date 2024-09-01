fe9d3f35a38ceede95ef5c43a97db1c9185f0ab2
prog-daiki <t.daiki0913@icloud.com>
2024-09-01 11:29:42 +0900

    モバイルメニューの修正

--- app/_components/mobile-menu.tsx
+++ app/_components/mobile-menu.tsx
@@ -1,13 +1,23 @@
+"use client";
+
 import { Menu } from "lucide-react";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import { Sidebar } from "../(main)/_components/sidebar";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 export const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
   return (
     <>
-      <Sheet>
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger className="pr-4 transition hover:bg-opacity-75 xl:hidden">
           <Menu />
         </SheetTrigger>
